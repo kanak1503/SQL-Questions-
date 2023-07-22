90e73ee848973744e1f5595287c84582965d000e
KANAK RATHI <121484266+kanak1503@users.noreply.github.com>
2023-07-23 00:21:03 +0530

    Create Employees With Missing Information.sql

---
+++ Employees With Missing Information.sql
@@ -0,0 +1,12 @@
+select e.employee_id
+    from Employees e
+    left join Salaries s
+    using(employee_id)
+    where salary is null
+union all
+select s.employee_id
+    from Employees e
+    right join Salaries s
+    using(employee_id)
+    where name is null
+order by employee_id;
